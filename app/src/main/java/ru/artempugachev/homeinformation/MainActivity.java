7c40afa231e2b0e2b38017b327333f7f68fac462
artem-p <artempugachev1@gmail.com>
2017-03-31 11:10:22 +0300

    hide status bar

--- app/src/main/java/ru/artempugachev/homeinformation/MainActivity.java
+++ app/src/main/java/ru/artempugachev/homeinformation/MainActivity.java
@@ -1,7 +1,9 @@
 package ru.artempugachev.homeinformation;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hideStatusBar();
+    }
+
+    private void hideStatusBar() {
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }
