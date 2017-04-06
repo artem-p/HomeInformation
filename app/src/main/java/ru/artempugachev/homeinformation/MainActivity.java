cc06a6246561c25b69ef6938b5f515e8c541998a
artem-p <artempugachev1@gmail.com>
2017-04-06 12:25:31 +0300

    methods for location output

--- app/src/main/java/ru/artempugachev/homeinformation/MainActivity.java
+++ app/src/main/java/ru/artempugachev/homeinformation/MainActivity.java
@@ -1,18 +1,34 @@
 package ru.artempugachev.homeinformation;
 
+import android.Manifest;
 import android.app.ActionBar;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private Location mLocation;
+    private GoogleApiClient mGoogleApiClient;
+    private final static int REQUEST_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +40,68 @@ public class MainActivity extends AppCompatActivity {
 
         TextView curWeatherTextView = (TextView) findViewById(R.id.curWeatherTextView);
         curWeatherTextView.setText(BuildConfig.DARK_SKY_API_KEY);
+
+        if (checkPlayServices()) {
+            buildGoogleApiClient();
+        }
+    }
+
+    private void displayLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //  request permissions. Then catch callback and get location there
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_LOCATION);
+        } else {
+            // permissions granted, get location
+            getLocation();
+        }
+    }
+
+    private void getLocation() {
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        outputLocation(location);
+    }
+
+    private void outputLocation(Location location) {
+        if (location != null) {
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+
+            String locationStr = lat + ", " + lon;
+
+            Toast.makeText(MainActivity.this, locationStr, Toast.LENGTH_SHORT).show();
+        } else {
+
+            Toast.makeText(MainActivity.this, R.string.cannot_get_location, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION: {
+                if(grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted. Get location.
+                    getLocation();
+                } else {
+                    // todo
+                    // location permission denied.
+                }
+                return;
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    private void buildGoogleApiClient() {
+    }
+
+    private boolean checkPlayServices() {
+        return false;
     }
 
     private void setUpDateView() {
@@ -37,5 +115,20 @@ public class MainActivity extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
