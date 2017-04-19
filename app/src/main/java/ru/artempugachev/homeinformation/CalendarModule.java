ab2584310a93540b687f9bf27e0973a7c9f7db27
artem-p <artempugachev1@gmail.com>
2017-04-19 18:20:18 +0300

    display first event for today

--- app/src/main/java/ru/artempugachev/homeinformation/CalendarModule.java
+++ app/src/main/java/ru/artempugachev/homeinformation/CalendarModule.java
@@ -7,6 +7,7 @@ import java.util.List;
 import me.everything.providers.android.calendar.Calendar;
 import me.everything.providers.android.calendar.CalendarProvider;
 import me.everything.providers.android.calendar.Event;
+import me.everything.providers.android.calendar.Instance;
 
 /**
  * Get data from calendar
@@ -22,11 +23,26 @@ public class CalendarModule {
 
         if (!calendars.isEmpty()) {
             Calendar calendar = calendars.get(0);
-            List<Event> events = calendarProvider.getEvents(calendar.id).getList();
 
-            if (!events.isEmpty()) {
-                String event = events.get(0).title;
-                return event;
+            java.util.Calendar begCalendar = java.util.Calendar.getInstance();
+            begCalendar.set(java.util.Calendar.HOUR_OF_DAY, 0);
+            begCalendar.set(java.util.Calendar.MINUTE, 0);
+            begCalendar.set(java.util.Calendar.SECOND, 0);
+            long beg = begCalendar.getTimeInMillis();
+
+            java.util.Calendar endCalendar = java.util.Calendar.getInstance();
+            endCalendar.set(java.util.Calendar.HOUR_OF_DAY, 23);
+            endCalendar.set(java.util.Calendar.MINUTE, 59);
+            endCalendar.set(java.util.Calendar.SECOND, 59);
+            long end = endCalendar.getTimeInMillis();
+
+
+            List<Instance> instances = calendarProvider.getInstances(beg, end).getList();
+            if (!instances.isEmpty()) {
+                Instance instance = instances.get(0);
+                Event event = calendarProvider.getEvent(instance.eventId);
+                String eventTitle = event.title;
+                return eventTitle;
             }
         }
         return null;
