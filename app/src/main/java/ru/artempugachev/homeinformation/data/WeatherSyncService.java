b1787e711deb349433e92f8b95a647afd4d0d316
Artem Pugachev <artempugachev1@gmail.com>
2017-08-31 23:41:12 +0300

    weather sync service to java begin

---
+++ app/src/main/java/ru/artempugachev/homeinformation/data/WeatherSyncService.java
@@ -0,0 +1,65 @@
+package ru.artempugachev.homeinformation.data;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+import kotlin.jvm.Synchronized;
+import ru.artempugachev.homeinformation.BuildConfig;
+import ru.artempugachev.homeinformation.weather.Coordinate;
+import ru.artempugachev.homeinformation.weather.DarkSkyProvider;
+import ru.artempugachev.homeinformation.weather.WeatherData;
+
+/**
+ * Service for weather sync
+ * It is subclass of {@link IntentService} so it will be on separate thread
+ * Get weather from weather provider and put in to database via content provider
+ */
+
+class WeatherSyncService extends IntentService {
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public WeatherSyncService(String name) {
+        super(name);
+    }
+
+    public WeatherSyncService() {
+        super("WeatherSyncService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        sync();
+    }
+
+    void sync() {
+        syncWeather(WeatherSyncService.this);
+    }
+
+    @Synchronized
+    private static void syncWeather(Context context) {
+        DarkSkyProvider darkSkyProvider = new DarkSkyProvider(BuildConfig.DARK_SKY_API_KEY);
+
+        // todo fetch for real coordinates
+        WeatherData weatherData = darkSkyProvider.fetchWeatherData(new Coordinate("59.93", "30.29"))
+
+        if (weatherData != null) {
+            val dataProvider = DataProvider(context)
+            dataProvider.deleteData()
+            val rowsInserted = dataProvider.writeWeather(weatherData)
+        }
+    }
+}
+
+
+
+
+fun startWeatherSyncNow(context: Context) {
+    val syncIntent = Intent(context, WeatherSyncService::class.java)
+    context.startService(syncIntent)
+}
+
