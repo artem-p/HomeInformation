22564b050ec9aa46444306163f7c10e3155c062c
Artem Pugachev <artempugachev1@gmail.com>
2017-09-01 23:26:03 +0300

    api interface stub

---
+++ app/src/main/java/ru/artempugachev/homeinformation/data/api/DarkSkyApiInterface.java
@@ -0,0 +1,10 @@
+package ru.artempugachev.homeinformation.data.api;
+
+/**
+ * Retrofit endpoints for darksky
+ */
+
+public interface DarkSkyApiInterface {
+    // todo query is https://api.darksky.net/forecast/c95722ae96802296ed9c6f4d57747b77/37.8267,-122.4233
+    // todo how to define endpoint for such a query
+}
